89b50b0ac8fc9f25a26ab3d606aa9339dc2c8a31
Nobuchika Tanaka <n.tanaka@idcf.jp>
2026-01-22 07:39:04 +0900

    DB接続を非同期化

--- python/api/db.py
+++ python/api/db.py
@@ -1,14 +1,18 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 DB_URL = "mysql+pymysql://root@db:3306/demo?charset=utf8"
+ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/demo?charset=utf8"
 
-db_engine = create_engine(DB_URL, echo=True)
-db_session = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
+async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
+
+async_session = sessionmaker(
+    autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession
+)
 
 Base = declarative_base()
 
 
-def get_db():
-    with db_session() as session:
+async def get_db():
+    async with async_session() as session:
         yield session
