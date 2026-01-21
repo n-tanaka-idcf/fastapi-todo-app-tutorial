99ffbf9195022bef57018dc37d0a701311a674e2
Nobuchika Tanaka <n.tanaka@idcf.jp>
2026-01-22 06:27:31 +0900

    タスク参照の処理を追加

--- python/api/cruds/task.py
+++ python/api/cruds/task.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+from sqlalchemy.engine import Result
 from sqlalchemy.orm import Session
 
 import api.models.task as task_model
@@ -10,3 +12,15 @@ def create_task(db: Session, task_create: task_schema.TaskCreate) -> task_model.
     db.commit()
     db.refresh(task)
     return task
+
+
+def get_tasks_with_done(db: Session) -> list[tuple[int, str, bool]]:
+    result: Result = db.execute(
+        select(
+            task_model.Task.id,
+            task_model.Task.title,
+            task_model.Done.id.isnot(None).label("done"),
+        ).outerjoin(task_model.Done)
+    )
+
+    return result.all()
