87baaedee2c83fd259791133569ffe65ada57806
Nobuchika Tanaka <n.tanaka@idcf.jp>
2026-01-21 17:57:08 +0900

    レスポンススキーマを追加

--- python/api/schemas/task.py
+++ python/api/schemas/task.py
@@ -1,7 +1,24 @@
 from pydantic import BaseModel, Field
 
 
-class Task(BaseModel):
-    id: int
+class TaskBase(BaseModel):
     title: str | None = Field(default=None, examples=["クリーニングを取りに行く"])
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskCreateResponse(TaskCreate):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class Task(TaskBase):
+    id: int
     done: bool = Field(default=False, description="完了フラグ")
+
+    class Config:
+        from_attributes = True
