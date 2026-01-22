e69333171294a69aca8eb18d89c9764b0cb50fb7
Nobuchika Tanaka <n.tanaka@idcf.jp>
2026-01-23 05:47:13 +0900

    modelの記法を修正

--- python/api/models/task.py
+++ python/api/models/task.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Date, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from datetime import date
+
+from sqlalchemy import Date, ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from api.db import Base
 
@@ -7,16 +9,16 @@ from api.db import Base
 class Task(Base):
     __tablename__ = "tasks"
 
-    id = Column(Integer, primary_key=True)
-    title = Column(String(1024))
-    due_date = Column(Date)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str | None] = mapped_column(String(1024))
+    due_date: Mapped[date | None] = mapped_column(Date)
 
-    done = relationship("Done", back_populates="task", cascade="delete")
+    done: Mapped["Done"] = relationship("Done", back_populates="task", cascade="delete")
 
 
 class Done(Base):
     __tablename__ = "dones"
 
-    id = Column(Integer, ForeignKey("tasks.id"), primary_key=True)
+    id: Mapped[int] = mapped_column(ForeignKey("tasks.id"), primary_key=True)
 
-    task = relationship("Task", back_populates="done")
+    task: Mapped["Task"] = relationship("Task", back_populates="done")
